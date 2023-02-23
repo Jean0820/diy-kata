5c078ac14447ee5165a1f5681dc03dbe8cef41b8
Jean-Marc Mufind <makayamufind@gmail.com>
2023-02-23 17:26:38 +0200

    reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
-
+const reachDestination = (distance, speed) => {
+    const time = distance / speed;
+    const formattedTime = time.toFixed(0);
+    return `I should be there in ${formattedTime} hours.`;
+  };
+  
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,12 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    const distance = 100; // km
+    const speed = 50; // km/h
+    const time = distance / speed;
+    const result = reachDestination(distance, speed);
+    const expected = `I should be there in ${time} hours.`;
+    expect(result).toEqual(expected);
+  });
 });
