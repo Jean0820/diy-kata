70a9fda207e20e16c7b6fabdc0122fd2040aa393
Jean-Marc Mufind <makayamufind@gmail.com>
2023-02-23 17:53:03 +0200

    humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,20 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    let humanYears = number;
+    let catYears, dogYears;
+    
+    if (number === 1) {
+      catYears = 15;
+      dogYears = 15;
+    } else if (number === 2) {
+      catYears = 24;
+      dogYears = 24;
+    } else {
+      catYears = 24 + 4 * (number - 2);
+      dogYears = 24 + 5 * (number - 2);
+    }
+  
+    return [humanYears, catYears, dogYears];
+  };
+  
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
 const { humanCatDogYears } = require("../src");
 
+describe("humanCatDogYears", () => {
+    test("returns human, cat and dog years based on input years", () => {
+        const inputAge = 10;
+        const result = humanCatDogYears(inputAge);
+        const expected = [10, 56, 64];
+        expect(result).toEqual(expected);
+      });      
+ });
 // Look Ma, no handlebars!!!
