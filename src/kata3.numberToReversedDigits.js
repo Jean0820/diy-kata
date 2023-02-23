08dcddb6e96aca52cd8fc7ec1a5824440ad37f2b
Jean-Marc Mufind <makayamufind@gmail.com>
2023-02-23 16:35:20 +0200

    numberToReversedDigits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,11 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+  const digits = [];
+  while (number > 0) {
+    digits.push(number % 10);
+    number = Math.floor(number / 10);
+  }
+  return digits;
+};
+
 
 module.exports = numberToReversedDigits;
