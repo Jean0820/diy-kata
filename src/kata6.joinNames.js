ae8c5a143e8f6a9f8bdd58f38f03434b937d143d
Jean-Marc Mufind <makayamufind@gmail.com>
2023-02-23 17:31:23 +0200

    joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,10 @@
-const joinNames = namesObj => {};
+const joinNames = (namesObj) => {
+    const names = namesObj.map((obj) => `${obj.firstName} ${obj.lastName}`);
+    const last = names.pop();
+    return names.length > 0
+      ? `${names.join(", ")} & ${last}`
+      : last;
+  };
+  
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,15 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, separated by commas and an ampersand", () => {
+    const names = [
+      { firstName: "John", lastName: "Doe" },
+      { firstName: "Jane", lastName: "Doe" },
+      { firstName: "Bob", lastName: "Smith" },
+    ];
+    const result = joinNames(names);
+    const expected = "John Doe, Jane Doe & Bob Smith";
+    expect(result).toEqual(expected);
+  });
+  
 });
